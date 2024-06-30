35078972b050c24d0ee7d0c42ef8b501c83e2bd4
Joseph Redmon <josephredmon@Josephs-MacBook-Pro.local>
2024-06-30 14:22:39 -0700

    first rust solution

--- exercises/chunk/chunkr/src/main.rs
+++ exercises/chunk/chunkr/src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
-    println!("Hello, world!");
+    let input: Vec<usize> = vec![1, 2, 3];
+    let size: usize = 2;
+    chunks(input, size);
+}
+
+fn chunks(v: Vec<usize>, size: usize) -> Vec<Vec<usize>> {
+    let mut res_vec: Vec<Vec<usize>> = Vec::new();
+
+    for num in v.iter() {
+        let mut curr_sub_vec = res_vec.last_mut();
+
+        match curr_sub_vec {
+            None => res_vec.push(vec![*num]),
+            Some(n) => {
+                if n.len() == size {
+                    res_vec.push(vec![*num])
+                } else {
+                    n.push(*num)
+                }
+            }
+        }
+    }
+
+    // println!("res_vec: {:?}", res_vec);
+    res_vec
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_output() {
+        let input: Vec<usize> = vec![1, 2, 3];
+        let expected: Vec<Vec<usize>> = vec![vec![1, 2], vec![3]];
+        assert_eq!(chunks(input, 2), expected);
+    }
 }
