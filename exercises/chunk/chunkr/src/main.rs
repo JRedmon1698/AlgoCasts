379daad805ccc23d7fe2a09c4c33a69c15c643ab
Joseph Redmon <josephredmon@Josephs-MacBook-Pro.local>
2024-06-30 15:05:28 -0700

    out of bounds indexing made rust solution 2 fuuuuuun

--- exercises/chunk/chunkr/src/main.rs
+++ exercises/chunk/chunkr/src/main.rs
@@ -4,25 +4,39 @@ fn main() {
     chunks(input, size);
 }
 
+// fn chunks(v: Vec<usize>, size: usize) -> Vec<Vec<usize>> {
+//     let mut res_vec: Vec<Vec<usize>> = Vec::new();
+
+//     for num in v.iter() {
+//         let mut curr_sub_vec = res_vec.last_mut();
+
+//         match curr_sub_vec {
+//             None => res_vec.push(vec![*num]),
+//             Some(n) => {
+//                 if n.len() == size {
+//                     res_vec.push(vec![*num])
+//                 } else {
+//                     n.push(*num)
+//                 }
+//             }
+//         }
+//     }
+
+//     // println!("res_vec: {:?}", res_vec);
+//     res_vec
+// }
+
 fn chunks(v: Vec<usize>, size: usize) -> Vec<Vec<usize>> {
     let mut res_vec: Vec<Vec<usize>> = Vec::new();
+    let mut sub_vec_marker: usize = 0;
 
-    for num in v.iter() {
-        let mut curr_sub_vec = res_vec.last_mut();
-
-        match curr_sub_vec {
-            None => res_vec.push(vec![*num]),
-            Some(n) => {
-                if n.len() == size {
-                    res_vec.push(vec![*num])
-                } else {
-                    n.push(*num)
-                }
-            }
-        }
+    while sub_vec_marker < v.len() as usize {
+        // ensure we dont try to index beyond the length of input vec
+        let end = std::cmp::min(sub_vec_marker + size, v.len());
+        res_vec.push(Vec::<usize>::from(&v[sub_vec_marker..end]));
+        sub_vec_marker += size;
     }
 
-    // println!("res_vec: {:?}", res_vec);
     res_vec
 }
 
