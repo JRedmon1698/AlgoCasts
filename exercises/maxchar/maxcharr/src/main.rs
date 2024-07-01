42cabb67b3c77109c97c921c544040b9e68f1c51
Joe Redmon <joe.redmon@emsibg.com>
2024-07-01 08:28:56 -0700

    maxchars done and tests

--- exercises/maxchar/maxcharr/src/main.rs
+++ exercises/maxchar/maxcharr/src/main.rs
@@ -2,6 +2,7 @@ use std::{char, collections::HashMap};
 
 fn main() {
     println!("Max char: {}", maxchars("abca".to_string()));
+    println!("Max char: {}", maxchars("abbba".to_string()));
 }
 
 fn maxchars(s: String) -> char {
@@ -21,8 +22,27 @@ fn maxchars(s: String) -> char {
             }
         };
     }
+    // println!("map: {:?}", char_map);
 
-    for val in char_map.values() {}
+    let mut max_char: char = char_vec[0];
+    let mut max_char_count: usize = 0;
+    for key in char_map.keys() {
+        if *char_map.get(key).unwrap() > max_char_count {
+            max_char_count = *char_map.get(key).unwrap();
+            max_char = *key;
+        }
+    }
 
-    'a'
+    max_char
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_output() {
+        assert_eq!(maxchars(String::from("abbcaa")), 'a');
+        assert_eq!(maxchars(String::from("12333")), '3');
+    }
 }
