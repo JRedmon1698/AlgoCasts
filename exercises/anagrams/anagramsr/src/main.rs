ddb53ab6960d38c1af9d0cedcb3a45d5429d6559
Joe Redmon <joesredmon@gmail.com>
2024-07-02 08:47:35 -0700

    anagramsr stub

--- exercises/anagrams/anagramsr/src/main.rs
+++ exercises/anagrams/anagramsr/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let string_1 = "Hello There!".to_string();
+    let string_2 = "threelolhe";
+
+    let res = anagrams(string_1, string_2);
 }
+
+fn anagrams(str_1: String, str_2: String) -> bool {}
