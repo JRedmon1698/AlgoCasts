11f8a4bf5fddd37e3983efd27cbb3559cf726ebd
Joe Redmon <joe.redmon@emsibg.com>
2024-07-08 13:05:20 -0700

    capitalize js simple solution

--- exercises/anagrams/anagramsr/src/main.rs
+++ exercises/anagrams/anagramsr/src/main.rs
@@ -1,8 +1,48 @@
 fn main() {
     let string_1 = "Hello There!".to_string();
-    let string_2 = "threelolhe";
+    let string_2 = "threelolhe".to_string();
 
-    let res = anagrams(string_1, string_2);
+    let res = anagrams(string_1.clone(), string_2.clone());
+
+    println!("{} is an anagram of {}? {}", string_1, string_2, res);
 }
 
-fn anagrams(str_1: String, str_2: String) -> bool {}
+fn anagrams(str_1: String, str_2: String) -> bool {
+    // split ea string into vec of chars
+    // filter out non-alpha chars
+    // map over vec and lower-case ea char
+    // sort vec
+    // compare
+
+    let split_vec_1: Vec<char> = str_1.chars().collect();
+    // println!("char vec: {:?}", split_vec_1);
+    let split_vec_2: Vec<char> = str_2.chars().collect();
+
+    let mut filtered_vec_1: Vec<char> = split_vec_1
+        .into_iter()
+        .filter(|c| c.is_alphabetic())
+        .collect();
+    let mut filtered_vec_2: Vec<char> = split_vec_2
+        .into_iter()
+        .filter(|c| c.is_alphabetic())
+        .collect();
+    // println!("alpha only vec: {:?}", filtered_vec_1);
+
+    let mut lower_filtered_vec_1: Vec<char> = filtered_vec_1
+        .iter_mut()
+        .map(|c| c.to_ascii_lowercase())
+        .collect();
+    lower_filtered_vec_1.sort();
+
+    let mut lower_filtered_vec_2: Vec<char> = filtered_vec_2
+        .iter_mut()
+        .map(|c| c.to_ascii_lowercase())
+        .collect();
+    lower_filtered_vec_2.sort();
+
+    // println!("lower filtered vec: {:?}", lower_filtered_vec_1);
+    let final_comp_string_1: String = lower_filtered_vec_1.into_iter().collect();
+    let final_comp_string_2: String = lower_filtered_vec_2.into_iter().collect();
+
+    final_comp_string_1 == final_comp_string_2
+}
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,28 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  // easy impl:
+  // split each sent at space
+  // iterate over arr of words
+  // iterate over each word
+  // capitalize first char
+  // join arr on space to create final string
+
+  let words = str.split(' ');
+  // console.log('arr of words: ', words);
+
+  for (i = 0; i < words.length; i++) {
+    let capFirstChar = words[i][0].toUpperCase();
+    let capWord = `${capFirstChar}${words[i].substr(1)}`;
+    words[i] = capWord;
+  }
+
+  return words.join(' ');
+}
+
+console.log(capitalize('i love breakfast at bill miller bbq'));
+console.log(capitalize('hi there, how is it going?'));
+
 
 module.exports = capitalize;
