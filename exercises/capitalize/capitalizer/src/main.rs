ddcab893aae22aff1bb76879c868dc858b16a174
Joe Redmon <joe.redmon@emsibg.com>
2024-07-08 15:29:44 -0700

    capitalize rust solution

--- exercises/capitalize/capitalizer/src/main.rs
+++ exercises/capitalize/capitalizer/src/main.rs
@@ -1,7 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let res = capitalize("why hello, there! babaganoush here.".to_string());
+    println!("{res}");
 }
 
 fn capitalize(s: String) -> String {
-    "ballast".to_string()
+    let split_str: Vec<String> = s.split(' ').map(|s| s.to_string()).collect();
+    // println!("split string: {:?}", split_str);
+
+    let mut capitalized_vec: Vec<String> = Vec::new();
+
+    for word in split_str.iter() {
+        let first_char: char = word.chars().nth(0).unwrap().to_ascii_uppercase();
+        // println!("first char: {}", first_char);
+
+        let capitalized_word = format!("{}{}", first_char, word[1..].to_string());
+        // println!("cap word: {capitalized_word}");
+        capitalized_vec.push(capitalized_word);
+    }
+
+    // println!("cap vec: {:?}", capitalized_vec);
+    capitalized_vec.join(" ")
 }
