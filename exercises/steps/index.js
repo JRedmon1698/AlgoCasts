bbdd7433daf217f4efa4b60e58384404a050a2dd
Joe Redmon <joe.redmon@emsibg.com>
2024-07-09 08:27:49 -0700

    nah recursion

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,46 +17,46 @@
 //       '### '
 //       '####'
 
-// function steps(n) {
-//   let str = '';
-//   let counter = 1;
-
-//   while (counter <= n) {
-//     str += '#';
-//     counter++;
-//   }
-
-//   let logStr = str;
-//   for (let i = str.length - 1; i >= 0; i--) {
-//     console.log(logStr.slice(i) + addSpace(i));
-//   }
-// }
-
-// function addSpace(n) {
-//   let str = '';
-//   let counter = 0;
-//   while (counter < n) {
-//     str += ' ';
-//     counter++;
-//   }
-
-//   return str;
-// }
-
 function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let stair = '';
+  let str = '';
+  let counter = 1;
 
-    for (let col = 0; col < n; col++) {
-      if (col <= row) {
-        stair += '#';
-      } else {
-        stair += ' ';
-      }
-    }
-    console.log(stair);
+  while (counter <= n) {
+    str += '#';
+    counter++;
+  }
+
+  let logStr = str;
+  for (let i = str.length - 1; i >= 0; i--) {
+    console.log(logStr.slice(i) + addSpace(i));
   }
 }
+
+function addSpace(n) {
+  let str = '';
+  let counter = 0;
+  while (counter < n) {
+    str += ' ';
+    counter++;
+  }
+
+  return str;
+}
+
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+
+//     for (let col = 0; col < n; col++) {
+//       if (col <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
 
 
 console.log(steps(3))
