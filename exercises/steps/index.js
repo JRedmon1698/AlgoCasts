cf16aa4bd7af70c6ea92a48450e3e30860673146
Joe Redmon <joe.redmon@emsibg.com>
2024-07-10 07:52:29 -0700

    php steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -25,6 +25,7 @@ function steps(n) {
     str += '#';
     counter++;
   }
+  console.log(str);
 
   let logStr = str;
   for (let i = str.length - 1; i >= 0; i--) {
--- exercises/steps/index.php
+++ exercises/steps/index.php
@@ -1,11 +1,30 @@
 <?php
 
 function steps($iNum){
-    
+    $iCounter = 1;
+    $sStr = '';
+
+    while($iCounter <= $iNum){
+        $sStr .= '#';
+        $iCounter++;
+    }
+
+    $sDisplayStr = $sStr;
+    for($i = strlen($sStr) - 1; $i >= 0; $i--){
+        var_dump(substr($sDisplayStr, $i) . addSpace($i));
+    }
 }
 
 function addSpace($iNum){
-    
+    $iCounter = 0;
+    $sSpaces = '';
+    while($iCounter < $iNum){
+        $sSpaces = $sSpaces .= ' ';
+        $iCounter++;
+    }
+
+    return $sSpaces;
 }
 
+// var_dump(addSpace(3));
 var_dump(steps(3));
