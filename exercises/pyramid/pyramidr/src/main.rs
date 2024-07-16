5a0a941ebcc1a05ee1d94b678e766ab47318a5ae
Joe Redmon <joe.redmon@emsibg.com>
2024-07-16 08:37:13 -0700

    rust pyramid, iterating is different in rust

--- exercises/pyramid/pyramidr/src/main.rs
+++ exercises/pyramid/pyramidr/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let res = pyramid(3);
+    let res = pyramid(7);
     println!("{:?}", res);
-    let spaces = add_spaces("#######".to_string(), 1);
-    println!("{}", spaces);
+    // let spaces = add_spaces("#######".to_string(), 3);
+    // println!("{}", spaces);
 }
 
 fn pyramid(n: usize) {
@@ -15,16 +15,24 @@ fn pyramid(n: usize) {
     for i in 0..base_length {
         base_string.push_str("#");
     }
+
+    let start_string = base_string.clone();
+    let halfway: f64 = (start_string.len() / 2) as f64;
+    let start_point = (halfway.floor() as usize);
+    for i in (0..=start_point).rev() {
+        let display_str = add_spaces(start_string.clone(), i);
+        println!("{}", display_str);
+    }
 }
 
 fn add_spaces(s: String, n: usize) -> String {
     let mut spaces = String::new();
-    for i in 0..n {
+    for _ in 0..n {
         spaces.push_str(" ");
     }
 
-    let length = s.len() - n;
-    let slice = &s[n..length];
+    let end = s.len() - n;
+    let slice = &s[n..end];
     let spaced_str: String = format!("{}{}{}", spaces, slice, spaces);
 
     spaced_str
