75c1c0f2852578e90882eef27c271b19a3830d25
Joe Redmon <joe.redmon@emsibg.com>
2024-07-17 08:00:33 -0700

    start js matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,29 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let matrix = [];
+  // for 1..n
+  // iterate over range adding each num to inner arr
+  // when innter arr reaches length n, start over
+  // fill first row
+  // fill last col
+  // fill last row
+  // fill first col - 1
+  // fill 2nd row - 1
+  // fill 2nd to last col - 2
+  // fill 2nd to last row - 2
+  let innerMatrix = [];
+  for (let row = 0; row < n; row++) {
+    for (let col = 0; col < n; col++) {
+      
+    }
+    matrix.push(innerMatrix);
+  }
+
+  return matrix;
+}
+
+console.log(matrix(3));
 
 module.exports = matrix;
