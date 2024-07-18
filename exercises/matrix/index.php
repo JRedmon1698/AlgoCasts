81c98d632062fa43e0a393ee92ee5f072fdddef9
Joe Redmon <joe.redmon@emsibg.com>
2024-07-18 08:07:40 -0700

    start notes for php matrix

---
+++ exercises/matrix/index.php
@@ -0,0 +1,21 @@
+<?php
+
+function matrix($iNum) {
+    // inst container
+    // iterate to iNum and fill container with arrays
+
+    // inst start/end col/row vars
+    // inst counter var
+
+    // start while loop to check start col/row <= end col/row
+        // for loop, incr cols, setting startRow
+            // incr startRow
+        // for loop, incr rows, setting endCol
+            // decr endCol
+        // for loop, decr cols, setting endRow
+            // decr endRow
+        // for loop, decr rows, setting startCol
+            // incr startCol
+}
+
+var_dump(matrix(3));
