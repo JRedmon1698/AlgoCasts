18625c57df61f741de61c6892772a7948c5b74e7
Joseph Redmon <josephredmon@Josephs-MacBook-Pro.local>
2024-07-20 20:45:19 -0700

    need to debug php spiral matrix

--- exercises/matrix/index.php
+++ exercises/matrix/index.php
@@ -1,21 +1,54 @@
 <?php
 
-function matrix($iNum) {
+function matrix($iNum){
     // inst container
+    $aContainer = [];
     // iterate to iNum and fill container with arrays
+    for($i = 0; $i < $iNum; $i++){
+        $aContainer[] = [];
+    }
 
-    // inst start/end col/row vars
-    // inst counter var
+    var_dump($aContainer);
+
+    // inst vars
+    $iNum = 1;
+    $iLeftCol = 0;
+    $iRightCol = 0;
+    $iTopRow = 0;
+    $iBottomRow = 0;
 
     // start while loop to check start col/row <= end col/row
+    while($iNum <= $iNum * $iNum){
         // for loop, incr cols, setting startRow
-            // incr startRow
+        for($i = $iLeftCol; $i <= $iRightCol; $i++){
+            $aContainer[$iTopRow][$i] = $iNum;
+            $iNum++;
+        }
+        $iTopRow++;
+
         // for loop, incr rows, setting endCol
-            // decr endCol
+        for($i = $iTopRow; $i <= $iBottomRow; $i++){
+            $aContainer[$i][$iRightCol] = $iNum;
+            $iNum++;
+        }
+        $iRightCol--;
+
         // for loop, decr cols, setting endRow
-            // decr endRow
+        for($i = $iRightCol; $i >= $iLeftCol; $i--){
+            $aContainer[$iBottomRow][$i] = $iNum;
+            $iNum++;
+        }
+        $iBottomRow--;
+
         // for loop, decr rows, setting startCol
-            // incr startCol
+        for($i = $iBottomRow; $i >= $iTopRow; $i--){
+            $aContainer[$i][$iLeftCol] = $iNum;
+            $iNum++;
+        }
+        $iLeftCol++;
+    }
+
+    return $aContainer;
 }
 
 var_dump(matrix(3));
