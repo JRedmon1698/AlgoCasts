c71bb7f78e48d2b35f1eb1a56c2946403ddf620e
Joe Redmon <joe.redmon@emsibg.com>
2024-07-26 11:23:17 -0700

    fib rust

---
+++ exercises/fib/fibr/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let res = fib(500);
+    println!("{res}");
+}
+
+// fn fib(n: usize) -> usize {
+//     if n < 2 {
+//         return n;
+//     }
+
+//     return fib(n - 1) + fib(n - 2);
+// }
+
+fn fib(n: usize) -> usize {
+    if n < 2 {
+        return n;
+    }
+
+    let mut current: usize = 0;
+    let mut previous: usize = 1;
+    let mut second_previous: usize = 0;
+
+    for i in 2..=n {
+        current = previous + second_previous;
+        second_previous = previous;
+        previous = current;
+    }
+
+    current
+}
