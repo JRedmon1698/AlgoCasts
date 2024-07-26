ba4e1ae416ab67d64c677e30acae6d3c260d861a
Joe Redmon <joe.redmon@emsibg.com>
2024-07-26 10:19:04 -0700

    added memoize func to js fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,28 +8,6 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-  if (n === 0) {
-    return 0;
-  }
-  if (n === 1) {
-    return 1;
-  }
-
-  let curr = 0;
-  let previous = 1;
-  let secondPrevious = 0;
-
-  for (let i = 2; i <= n; i++) {
-    console.log(curr)
-    curr = previous + secondPrevious;
-    secondPrevious = previous;
-    previous = curr;
-  }
-
-  return curr;
-}
-
 // function fib(n) {
 //   if (n === 0) {
 //     return 0;
@@ -38,8 +16,48 @@ function fib(n) {
 //     return 1;
 //   }
 
-//   return fib(n - 1) + fib(n - 2);
+//   let curr = 0;
+//   let previous = 1;
+//   let secondPrevious = 0;
+
+//   for (let i = 2; i <= n; i++) {
+//     // console.log(curr)
+//     curr = previous + secondPrevious;
+//     secondPrevious = previous;
+//     previous = curr;
+//   }
+
+//   return curr;
 // }
 
-console.log(fib(6))
+function fib(n) {
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+function memoize(func) {
+  let cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    let result = func.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  }
+}
+
+fib = memoize(fib);
+
+console.time('fib');
+console.log(fib(50));
+console.timeEnd('fib');
 module.exports = fib;
