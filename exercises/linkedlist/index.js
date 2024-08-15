65131e90df7eb2a84b7884cc9a83f5670f3a5e1f
Joe Redmon <joe.redmon@emsibg.com>
2024-08-15 13:00:17 -0700

    js linked list WIP

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,46 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.nodes = [];
+    this.head = null;
+    this.tail = null;
+  }
+
+  insertFirst(data) {
+    let node = new Node(data);
+    this.head = node;
+    this.nodes.push(node);
+    // nope. need to cover the case where there is only one node 
+    // its head AND tail
+  }
+
+  size() {
+    return this.nodes.length;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    return this.tail;
+  }
+}
 
 module.exports = { Node, LinkedList };
+
+let l = new LinkedList();
+// console.log('empty ll: ', l);
+l.insertFirst('one');
+l.insertFirst('two');
+// console.log(l);
+// console.log(l.size())
