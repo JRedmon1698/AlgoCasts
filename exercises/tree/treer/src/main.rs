eaed68708544e477aeeb2a15a9508ffc08b2f54a
Joe Redmon <joe.redmon@emsibg.com>
2024-09-18 12:20:31 -0700

    traverse df and bf treer, rust

--- exercises/tree/treer/src/main.rs
+++ exercises/tree/treer/src/main.rs
@@ -31,11 +31,35 @@ impl Tree {
         Tree { root: None }
     }
 
-    fn traverse_bf<F>(&self, func: F)
+    fn traverse_bf<F>(&self, mut func: F)
     where
-        F: FnMut() -> (),
+        F: FnMut(&Node) -> (),
     {
-        let node = self.root.as_ref().unwrap();
+        let mut nodes = vec![self.root.as_ref().unwrap()];
+        while nodes.len() > 0 {
+            let node = nodes.remove(0);
+            for child in node.children.iter() {
+                nodes.push(child);
+            }
+
+            func(node);
+        }
+    }
+
+    fn traverse_df<F>(&self, mut func: F)
+    where
+        F: FnMut(&Node) -> (),
+    {
+        let mut nodes = vec![self.root.as_ref().unwrap()];
+        while nodes.len() > 0 {
+            let last_idx = nodes.len() - 1;
+            let node = nodes.remove(last_idx);
+            for child in node.children.iter() {
+                nodes.push(child);
+            }
+
+            func(node);
+        }
     }
 }
 
@@ -43,9 +67,10 @@ fn main() {
     let mut node = Node::new(5);
     node.add(7);
     node.add(9);
-    node.remove(9);
+    // node.remove(9);
 
     let mut tree = Tree::new();
     tree.root = Some(node);
+    tree.traverse_df(|n| println!("data + 1: {:?}", n.data.clone() + 1));
     println!("{:?}", tree);
 }
