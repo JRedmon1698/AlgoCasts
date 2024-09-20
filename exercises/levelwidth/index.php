ecf8fb627f470e5fbaf6cd31a0806c1fe81de7e2
Joseph Redmon <josephredmon@Josephs-MacBook-Pro.local>
2024-09-19 18:19:59 -0700

    php levelwidth done

--- exercises/levelwidth/index.php
+++ exercises/levelwidth/index.php
@@ -14,8 +14,31 @@ class Node {
     }
 }
 
+function levelWidth($root){
+    $counts = [0];
+    $stop = 'ballast';
+    $nodes = [$root, $stop];
+
+    while(count($nodes) > 1){
+        $node = array_shift($nodes);
+
+        if($node === $stop){
+            array_push($counts, 0);
+            array_push($nodes, $stop);
+        }
+        else {
+            array_push($nodes, ...$node->children);
+            $counts[count($counts) - 1]++;
+        }
+    }
+    return $counts;
+}
+
+
 $node = new Node(1);
 $node->add(3);
 $node->add(5);
 var_dump($node);
 
+$res = levelWidth($node);
+var_dump($res);
