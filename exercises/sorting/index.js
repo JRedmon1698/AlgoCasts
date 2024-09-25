52acecbfa48f01fd0bb2171930f580b5be5ba99a
Joe Redmon <joe.redmon@emsibg.com>
2024-09-25 10:05:00 -0700

    js bubble sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,7 +2,16 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let k = 0; k < arr.length - i; k++) {
+      if (arr[k] > arr[k + 1]) {
+        let prev = arr[k];
+        arr[k] = arr[k + 1];
+        arr[k + 1] = prev;
+      }
+    }
+  }
+  return arr;
 }
 
 function selectionSort(arr) {
@@ -18,3 +27,7 @@ function merge(left, right) {
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
+
+let arr = [4, -1, 10, 3, 7];
+let sorted = bubbleSort(arr);
+console.log(sorted);
