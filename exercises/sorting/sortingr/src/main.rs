acb287dcf914d9770887c4ae933faaffdcb0eabf
Joe Redmon <joe.redmon@emsibg.com>
2024-09-26 12:38:33 -0700

    rust selection sort

--- exercises/sorting/sortingr/src/main.rs
+++ exercises/sorting/sortingr/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let mut v: Vec<u8> = vec![10, 0, 2, 5, 1, 17, 9];
-    let res = bubble_sort(v.as_mut());
+    let v: Vec<u8> = vec![10, 0, 2, 5, 1, 17, 9];
+    let res = selection_sort(v);
     println!("sorted: {:?}", res);
 }
 
@@ -10,6 +10,22 @@ fn bubble_sort(v: &mut Vec<u8>) -> &Vec<u8> {
             if v[k] > v[k + 1] {
                 v.swap(k, k + 1);
             }
+        }
+    }
+
+    v
+}
+
+fn selection_sort(mut v: Vec<u8>) -> Vec<u8> {
+    for i in 0..v.len() {
+        let mut idx_of_min = i;
+        for k in i + 1..v.len() {
+            if v[k] < v[idx_of_min] {
+                idx_of_min = k;
+            }
+        }
+        if idx_of_min != i {
+            v.swap(idx_of_min, i);
         }
     }
 
