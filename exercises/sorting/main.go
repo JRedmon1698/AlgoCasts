dbc33e2c387bd3475f4c6a1e1085cd60aa4faa22
Joe Redmon <joe.redmon@emsibg.com>
2026-03-04 15:27:07 -0800

    go insertion sort

---
+++ exercises/sorting/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	ints := []int{2, 3, 4, 1, 6}
+	res := insertionSort(ints)
+	fmt.Println(res)
+}
+
+func insertionSort(nums []int) []int {
+	for i := 1; i < len(nums); i++ {
+		j := i - 1
+
+		for j >= 0 && nums[j+1] < nums[j] {
+			tmp := nums[j+1]
+			nums[j+1] = nums[j]
+			nums[j] = tmp
+			j--
+		}
+	}
+
+	return nums
+}
