e81a96eaea9727a0ceab4bef79f564fc7a9a4f92
Joe Redmon <joe.redmon@emsibg.com>
2026-03-06 13:32:15 -0800

    go merge sort

--- exercises/sorting/main.go
+++ exercises/sorting/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	ints := []int{2, 3, 4, 1, 6}
-	res := insertionSort(ints)
+	ints := []int{2, 3, 4, 1, 6, 5}
+	res := mergeSort(ints)
 	fmt.Println(res)
 }
 
@@ -21,4 +21,42 @@ func insertionSort(nums []int) []int {
 	}
 
 	return nums
+}
+
+func mergeSort(nums []int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+
+	mid := len(nums) / 2
+	left := mergeSort(nums[:mid])
+	right := mergeSort(nums[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	sorted := []int{}
+
+	i := 0
+	j := 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			sorted = append(sorted, left[i])
+			i++
+		} else {
+			sorted = append(sorted, right[j])
+			j++
+		}
+	}
+
+	if i < len(left) {
+		sorted = append(sorted, left[i:]...)
+	}
+	if j < len(right) {
+		sorted = append(sorted, right[j:]...)
+	}
+
+	return sorted
 }
